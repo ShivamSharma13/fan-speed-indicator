f9798d5f3d0b68e53ae7286e3500ae13dc2e745c
Shivam <mailtoshivam13@gmail.com>
2018-03-06 04:13:51 +0530

    Added ctrl-c signal recieving

--- indicator.py
+++ indicator.py
@@ -8,13 +8,28 @@ from gi.repository import AppIndicator3 as appindicator
 
 import signal
 
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 APPINDICATOR_ID = 'myappindicator'
 
 def main():
-	indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('icon.svg') , appindicator.IndicatorCategory.SYSTEM_SERVICES)
-	indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-	indicator.set_menu(gtk.Menu())
-	gtk.main()
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('fan.svg') , appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+    indicator.set_menu(build_menu(indicator))
+    gtk.main()
+
+
+def build_menu(indicator):
+    menu = gtk.Menu()
+    item_quit = gtk.MenuItem('Quit')
+    item_quit.connect('activate', quit)
+    menu.append(item_quit)
+    menu.show_all()
+    return menu
+ 
+
+def quit(source):
+    gtk.main_quit()
+
 
 if __name__ == "__main__":
-	main()
+    main()
