b377d34b1d369417554ff7fd31a16413e44107e6
Shivam <mailtoshivam13@gmail.com>
2018-03-06 04:06:28 +0530

    First Prototype

---
+++ indicator.py
@@ -0,0 +1,20 @@
+import gi
+import os
+gi.require_version('Gtk' , '3.0')
+gi.require_version('AppIndicator3', '0.1')
+
+from gi.repository import Gtk as gtk 
+from gi.repository import AppIndicator3 as appindicator 
+
+import signal
+
+APPINDICATOR_ID = 'myappindicator'
+
+def main():
+	indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('icon.svg') , appindicator.IndicatorCategory.SYSTEM_SERVICES)
+	indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+	indicator.set_menu(gtk.Menu())
+	gtk.main()
+
+if __name__ == "__main__":
+	main()
